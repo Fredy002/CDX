6225b537d7964ed956b89db7a532583bff42b417
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:40:46 -0500

    chore: Update next.config.js with image remote patterns and homepage redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'tiendasishop.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.notebookgalerie.de',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img10.naventcdn.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
