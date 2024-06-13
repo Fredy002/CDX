1f8a39c6edbc687c322908d70b6eebd6114baf0e
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:52:15 -0500

    feat: Add planes.tsx file with array of subscription plans

---
+++ utils/planes.tsx
@@ -0,0 +1,107 @@
+export const planes = [
+    {
+        id: 1,
+        title: "Básico",
+        price: "100",
+        subtitle: "",
+        features: [
+            "Gana de todos sus referidos directos",
+            "Participa del BUILDER POOL",
+            "Exclusividad de participar en cualquier tipo de inmueble",
+            "Primicias de proyectos inmobiliarios con ROI del 5% trimestral",
+            "6% de descuento en la compra de un departamento o 5% en una casa"
+        ],
+        buttonLabel: "Elegir Plan",
+        buttonLink: "/dashboard/suscripcion"
+    },
+    {
+        id: 2,
+        title: "Junior",
+        price: "500",
+        subtitle: "",
+        features: [
+            "Gana de todos sus referidos directos e indirectos hasta en un 2do nivel de profundidad",
+            "Participa del BUILDER POOL",
+            "Exclusividad de participar en cualquier tipo de inmueble",
+            "Primicias de proyectos inmobiliarios con ROI del 10% trimestral",
+            "9% de descuento en la compra de un departamento o 8% en una casa"
+        ],
+        buttonLabel: "Elegir Plan",
+        buttonLink: "/dashboard/suscripcion"
+    },
+    {
+        id: 3,
+        title: "Corredor inmobiliario",
+        price: "2000",
+        subtitle: "",
+        features: [
+            "Gana de todos sus referidos directos e indirectos hasta en un 3er nivel de profundidad",
+            "Participa del BUILDER POOL",
+            "Exclusividad de participar en cualquier tipo de inmueble",
+            "Primicias de proyectos inmobiliarios con ROI del 12% trimestral",
+            "12% de descuento en la compra de un departamento o 10% en una casa"
+        ],
+        buttonLabel: "Elegir Plan",
+        buttonLink: "/dashboard/suscripcion"
+    },
+    {
+        id: 4,
+        title: "Asesor inmobiliario",
+        price: "7000",
+        subtitle: "",
+        features: [
+            "Gana de todos sus referidos directos e indirectos hasta en un 4to nivel de profundidad",
+            "Participa del BUILDER POOL",
+            "Exclusividad de participar en cualquier tipo de inmueble",
+            "Primicias de proyectos inmobiliarios con ROI del 14% trimestral",
+            "15% de descuento en la compra de un departamento o 13% en una casa"
+        ],
+        buttonLabel: "Elegir Plan",
+        buttonLink: "/dashboard/suscripcion"
+    },
+    {
+        id: 5,
+        title: "Administrador Inmobiliario",
+        price: "25000",
+        subtitle: "",
+        features: [
+            "Gana de todos sus referidos directos e indirectos hasta en un 5to nivel de profundidad",
+            "Participa del BUILDER POOL",
+            "Exclusividad de participar en cualquier tipo de inmueble",
+            "Primicias de proyectos inmobiliarios con ROI del 25% trimestral",
+            "18% de descuento en la compra de un departamento o 16% en una casa"
+        ],
+        buttonLabel: "Elegir Plan",
+        buttonLink: "/dashboard/suscripcion"
+    },
+    {
+        id: 6,
+        title: "Gerente Inmobiliario",
+        price: "50000",
+        subtitle: "",
+        features: [
+            "Gana de todos sus referidos directos e indirectos hasta en un 6to nivel de profundidad",
+            "Participa del BUILDER POOL",
+            "Exclusividad de participar en cualquier tipo de inmueble",
+            "Primicias de proyectos inmobiliarios con ROI del 30% trimestral",
+            "22% de descuento en la compra de un departamento o 18% en una casa"
+        ],
+        buttonLabel: "Elegir Plan",
+        buttonLink: "/dashboard/suscripcion"
+    },
+    {
+        id: 7,
+        title: "Director Inmobiliario",
+        price: "100000",
+        subtitle: "",
+        features: [
+            "Gana de todos sus referidos directos e indirectos hasta en un 7mo nivel de profundidad",
+            "Participa del BUILDER POOL",
+            "Exclusividad de participar en cualquier tipo de inmueble",
+            "Primicias de proyectos inmobiliarios con ROI del 40% trimestral",
+            "25% de descuento en la compra de un departamento o 22% en una casa"
+        ],
+        buttonLabel: "Elegir Plan",
+        buttonLink: "/dashboard/suscripcion"
+    }
+];
