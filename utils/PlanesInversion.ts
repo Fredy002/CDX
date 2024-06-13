74adaf03334484acf1adc0ac3725fa7f258973c0
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:51:40 -0500

    feat: Add investment plan data files and components

---
+++ utils/PlanesInversion.ts
@@ -0,0 +1,91 @@
+export const planBasico = [
+    {
+      id: 1,
+      tokens: "2",
+      tokensImage: "/images/logo.png",
+      builderPool: "Sistema de referidos",
+      roi: "5%",
+      discount: "5%",
+      plan:"Básico"
+      
+    },
+];
+
+export const planJunior = [
+    {
+      id: 1,
+      tokens: "10",
+      tokensImage: "",
+      builderPool:"Sistema de referidos",
+      roi:"10%",
+      discount: "9%",
+      plan:"Junior"
+
+    },
+  ];
+
+export const planCorredor = [
+    {
+      id: 1,
+      tokens: "40",
+      tokensImage: "",
+      builderPool:"Sistema de referidos",
+      roi:"12%",
+      discount: "12%",
+      plan:"Corredor inmobiliario"
+
+    },
+  ];
+
+export const planAsesor = [
+    {
+      id: 1,
+      tokens: "70",
+      tokensImage: "",
+      builderPool:"Sistema de referidos",
+      roi:"14%",
+      discount: "13%",
+      plan:"Asesor inmobiliario"
+
+    },
+  ];
+
+export const planAdministrador = [
+    {
+      id: 1,
+      tokens: "250",
+      tokensImage: "",
+      builderPool:"Sistema de referidos",
+      roi:"25%",
+      discount: "16%",
+      plan:"Administrador inmobiliario"
+
+    },
+  ];
+
+export const planGerente = [
+    {
+      id: 1,
+      tokens: "500",
+      tokensImage: "",
+      builderPool:"Sistema de referidos",
+      roi:"30%",
+      discount: "22%",
+      plan:"Gerente inmobiliario"
+
+    },
+  ];
+
+export const planDirector = [
+    {
+      id: 1,
+      tokens: "1000",
+      tokensImage: "",
+      builderPool:"Sistema de referidos",
+      roi:"40%",
+      discount: "25%",
+      plan:"Director inmobiliario"
+
+    },
+  ];
+
