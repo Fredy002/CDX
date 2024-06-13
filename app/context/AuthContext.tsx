964528372a5fbc1402bb499a5a4f6e5a08c30eaa
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:41:49 -0500

    chore: Add AuthContext for managing user authentication

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+
+interface AuthContextType {
+    user: any;
+    setUser: (user: any) => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<any>(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, setUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+};
