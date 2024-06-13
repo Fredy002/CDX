9f7739703b9b19edc7ad19612f078e67f2b7bb03
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:51:21 -0500

    feat: Add comodidades.tsx file with array of amenities

---
+++ utils/comodidades.tsx
@@ -0,0 +1,18 @@
+export const comodidades = [
+    { id: 0, item: "A/C y Calefacción" },
+    { id: 1, item: "Garajes" },
+    { id: 2, item: "Piscina" },
+    { id: 3, item: "Estacionamiento" },
+    { id: 4, item: "Vista al lago" },
+    { id: 5, item: "Jardín" },
+    { id: 6, item: "Acceso para discapacitados" },
+    { id: 7, item: "Se admiten mascotas" },
+    { id: 8, item: "Altura del techo" },
+    { id: 9, item: "Ducha al aire libre" },
+    { id: 10, item: "Refrigerador" },
+    { id: 11, item: "Chimenea" },
+    { id: 12, item: "Wifi" },
+    { id: 13, item: "TV por cable" },
+    { id: 14, item: "Barbacoa" },
+  ];
+  
