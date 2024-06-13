f1278a8daca54ef371400de09a88c4b5b5da3f44
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:52:19 -0500

    feat: Add reviews.tsx file with array of reviews

---
+++ utils/reviews.tsx
@@ -0,0 +1,11 @@
+export const reviews = [
+    {
+        id: 1,
+        url: 2,
+        usuario: "Pedro Flores",
+        fecha: "17 Aug, 23",
+        descripcion: "Lorem ipsum dolor sit amet consectetur. Pellentesque sed nulla facili diam posuere aliquam suscipit quam.",
+        calificacion: "(4.7 Rating)",
+    }
+
+]
