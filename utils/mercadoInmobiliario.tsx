601d2ef29ad6ebfd130805cdd300b5d9c1e066a9
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:52:10 -0500

    feat: Add mercadoInmobiliario.tsx file with market data array

---
+++ utils/mercadoInmobiliario.tsx
@@ -0,0 +1,14 @@
+export const mercadoInmobiliario = [
+  {
+    title: "Mercado Inmobiliario",
+    description:
+  `Aumento de la urbanización a nivel latino-americano (LATAM 0.9% de crecimiento poblacional).
+
+    Valorización constante de las propiedades a nivel internacional (9-15% anual).
+    
+    Proyección de crecimiento del mercado inmobiliario`,
+    link: "/about-us",
+  },
+  
+  
+];
