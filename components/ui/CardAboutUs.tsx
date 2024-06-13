5f382b669fdb336c356a7bd8e80917435fa6c4b5
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:49:41 -0500

    feat: Add CardAboutUs component for displaying information about the company

---
+++ components/ui/CardAboutUs.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { Highlight } from '@/components/ui/Highlight';
+
+
+export const CardAboutUs = [
+    {
+        id: 0,
+        name: "",
+        designation: "",
+        content: (
+            <p>
+                Facilidad de inversion desde montos reducidos
+            </p>
+        ),
+    },
+    {
+        id: 1,
+        name: "",
+        designation: "",
+        content: (
+            <p>
+                Sin procesos engorrosos
+            </p>
+        ),
+    },
+    {
+        id: 2,
+        name: "",
+        designation: "",
+        content: (
+            <p>
+                Mayor seguridad
+            </p>
+        ),
+    },
+    {
+        id: 3,
+        name: "",
+        designation: "",
+        content: (
+            <p>
+                Retornos mensuales
+            </p>
+        ),
+    },
+];
