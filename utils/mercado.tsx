a8d7ab33b3e726847dfc42ef70364a38d2b8c0f6
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:52:04 -0500

    feat: Add mercado.tsx file with array of market data

---
+++ utils/mercado.tsx
@@ -0,0 +1,25 @@
+import { image } from "@nextui-org/react";
+
+export const mercado = [
+  {
+    title: "5.5 billones",
+    description:
+      "Disposición a invertir 56% (mayor a la media)",
+    link: "",
+    image: "",
+  },
+  {
+    title: "12.8 billones",
+    description:
+      "América Latina y el Caribe (en base a la distribución del PIB)",
+    link: "",
+    image: "",
+  },
+  {
+    title: "USD 160 billones",
+    description:
+      "Bienes raices residenciales",
+    link: "",
+    image: "",
+  },
+];
