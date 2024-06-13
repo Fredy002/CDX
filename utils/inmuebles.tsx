6e30db96333a146b573783c518fb26381229a6aa
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-12 20:51:59 -0500

    feat: Add inmuebles.tsx file with array of properties

---
+++ utils/inmuebles.tsx
@@ -0,0 +1,40 @@
+export const inmuebles = [
+    {
+        id: 1,
+        imagen: 'https://img10.naventcdn.com/avisos/111/01/43/42/52/16/360x266/1458685595.jpg?rapc=bXZhX2ltYWdl?isFirstImage=true',
+        tipo: 'CASA',
+        ubicacion: 'Colegio San Jerónimo, Arequipa, AR',
+        area: '735 m² tot.',
+        habitaciones: 'N/A',
+        baños: 'N/A',
+        estacionamientos: '3 estac.',
+        precio: '550,000',
+        descripcion: 'Casa de 2 pisos frente a la Universidad Católica. Cochera para 3 vehículos y amplios jardines. Excelente ubicación cerca de avenidas principales y universidades.',
+        fecha: '1 Jan, 2024',
+        coordenadas: {
+            lat: -16.4090301,
+            lng: -71.5503877
+        },
+        view: 10,
+        status: 'Active'
+    },
+    {
+        id: 2,
+        imagen: 'https://img10.naventcdn.com/avisos/11/00/66/58/08/93/360x266/363807045.jpg?isFirstImage=true',
+        tipo: 'CASA',
+        ubicacion: 'Umacollo, Yanahuara, Arequipa, AR',
+        area: 'N/A',
+        habitaciones: '4 dorm.',
+        baños: '2 baños',
+        estacionamientos: 'N/A',
+        precio: '260,000',
+        descripcion: 'Casa en Umacollo, ideal para vivienda o proyecto. Amoblada, cerca de la Universidad Católica. Sala comedor, cocina equipada, 4 dormitorios, 2 baños completos y patio.',
+        fecha: '13 Jan, 2024',
+        coordenadas: {
+            lat: -16.4069629,
+            lng: -71.5488015
+        },
+        view: 12,
+        status: 'Pending'
+    }
+];
