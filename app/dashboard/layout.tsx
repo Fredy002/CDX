54c00fe92cb8956d70964a685982ff9714679c22
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-13 19:55:24 -0500

    Update DashboardLayout component to improve responsiveness and layout structure

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,11 +4,11 @@ import UserNavBar from "@/components/dashboard/UserNavBar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full h-full bg-zinc-900">
-      <div className="w-full h-full flex justify-center items-center p-0">
-        <UserSideBar />
-        <div className="flex-1 flex flex-col h-screen">
-          <UserNavBar />
+    <div className="w-full h-screen bg-zinc-900 flex">
+      <UserSideBar />
+      <div className="flex-1 flex flex-col h-full">
+        <UserNavBar />
+        <div className="flex-1 overflow-auto">
           {children}
         </div>
       </div>
