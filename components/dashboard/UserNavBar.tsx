65dd8d18b8a80bc695ed8bc7e9154562ffd759a8
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-14 19:51:37 -0500

    style: Improve UserNavBar component styles and handle link clicks

--- components/dashboard/UserNavBar.tsx
+++ components/dashboard/UserNavBar.tsx
@@ -15,9 +15,12 @@ const UserNavBar = () => {
     setMenuVisible(!menuVisible);
   };
 
+  const handleLinkClick = () => {
+    setMenuVisible(false);
+  };
+
   return (
     <div className="w-full h-[80px] flex items-center justify-between bg-background">
-
       <div className="md:hidden flex items-center justify-center w-full md:h-full">
         <button
           className="flex text-white font-bold bg-[#0EA5E9] rounded-full shadow-lg p-4 z-50"
@@ -27,17 +30,17 @@ const UserNavBar = () => {
         </button>
         {menuVisible && (
           <div className="absolute top-[80px] left-0 w-full bg-black shadow-lg rounded-lg p-4 z-50 overflow-y-hidden">
-            <div className='flex flex-col items-center justify-center z-50 bg-black  bg-opacity-5 backdrop-blur h-screen gap-2.5 overflow-y-hidden'>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard"}>Dashboard</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/perfil"}>Perfil</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/suscripcion"}>Suscripcion</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/ajustes"}>Ajustes</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/properties"}>Mis Propiedades</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/add-propertie"}>Añadir Propiedad</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/favorite"}>Favoritos</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/saved-search"}>Busqueda Guardada</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/review"}>Comentarios</Link>
-              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/home/auth"}>Log-out</Link>
+            <div className='flex flex-col items-center justify-center z-50 bg-black bg-opacity-5 backdrop-blur h-screen gap-2.5 overflow-y-hidden'>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard"} onClick={handleLinkClick}>Dashboard</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/perfil"} onClick={handleLinkClick}>Perfil</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/suscripcion"} onClick={handleLinkClick}>Suscripcion</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/ajustes"} onClick={handleLinkClick}>Ajustes</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/properties"} onClick={handleLinkClick}>Mis Propiedades</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/add-propertie"} onClick={handleLinkClick}>Añadir Propiedad</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/favorite"} onClick={handleLinkClick}>Favoritos</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/saved-search"} onClick={handleLinkClick}>Busqueda Guardada</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/dashboard/review"} onClick={handleLinkClick}>Comentarios</Link>
+              <Link className='hover:bg-zinc-900 p-3 rounded-full' href={"/home/auth"} onClick={handleLinkClick}>Log-out</Link>
             </div>
           </div>
         )}
@@ -53,7 +56,7 @@ const UserNavBar = () => {
           <input
             type="text"
             name="search"
-            className="  py-2 pl-8 pr-4 bg-zinc-800 border-[1px] text-white border-zinc-600 focus:border-zinc-300 outline-none rounded-2xl"
+            className="py-2 pl-8 pr-4 bg-zinc-800 border-[1px] text-white border-zinc-600 focus:border-zinc-300 outline-none rounded-2xl"
             placeholder="Search here..."
           />
         </div>
