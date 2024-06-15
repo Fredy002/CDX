db9f8531e8a45815d5f5a756e39d6b2f22ab601d
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-14 19:44:44 -0500

    style: Update CardPerfil component styles for better mobile view

--- utils/planes.tsx
+++ utils/planes.tsx
@@ -12,7 +12,8 @@ export const planes = [
             "6% de descuento en la compra de un departamento o 5% en una casa"
         ],
         buttonLabel: "Elegir Plan",
-        buttonLink: "/dashboard/suscripcion"
+        buttonLink: "/dashboard/suscripcion",
+        description: "El plan Básico permite ganar de todos tus referidos directos y participar en el BUILDER POOL. Ofrece exclusividad para participar en cualquier tipo de inmueble y primicias de proyectos inmobiliarios con un ROI del 5% trimestral. Además, obtén un 6% de descuento en la compra de un departamento o un 5% en una casa."
     },
     {
         id: 2,
@@ -27,7 +28,8 @@ export const planes = [
             "9% de descuento en la compra de un departamento o 8% en una casa"
         ],
         buttonLabel: "Elegir Plan",
-        buttonLink: "/dashboard/suscripcion"
+        buttonLink: "/dashboard/suscripcion",
+        description: "Con el plan Junior, gana de todos tus referidos directos e indirectos hasta un 2do nivel de profundidad. Participa en el BUILDER POOL con exclusividad para cualquier tipo de inmueble. Benefíciate de un ROI del 10% trimestral y obtén un 9% de descuento en departamentos o un 8% en casas."
     },
     {
         id: 3,
@@ -42,7 +44,8 @@ export const planes = [
             "12% de descuento en la compra de un departamento o 10% en una casa"
         ],
         buttonLabel: "Elegir Plan",
-        buttonLink: "/dashboard/suscripcion"
+        buttonLink: "/dashboard/suscripcion",
+        description: "El plan Corredor inmobiliario te permite ganar de referidos directos e indirectos hasta un 3er nivel de profundidad. Accede al BUILDER POOL y disfruta de exclusividad en inmuebles. Aprovecha un ROI del 12% trimestral y un 12% de descuento en departamentos o un 10% en casas."
     },
     {
         id: 4,
@@ -57,7 +60,8 @@ export const planes = [
             "15% de descuento en la compra de un departamento o 13% en una casa"
         ],
         buttonLabel: "Elegir Plan",
-        buttonLink: "/dashboard/suscripcion"
+        buttonLink: "/dashboard/suscripcion",
+        description: "El plan Asesor inmobiliario ofrece ganancias de referidos directos e indirectos hasta un 4to nivel de profundidad. Participa en el BUILDER POOL con exclusividad en cualquier inmueble. Disfruta de un ROI del 14% trimestral y un 15% de descuento en departamentos o un 13% en casas."
     },
     {
         id: 5,
@@ -72,7 +76,8 @@ export const planes = [
             "18% de descuento en la compra de un departamento o 16% en una casa"
         ],
         buttonLabel: "Elegir Plan",
-        buttonLink: "/dashboard/suscripcion"
+        buttonLink: "/dashboard/suscripcion",
+        description: "Con el plan Administrador Inmobiliario, gana de referidos directos e indirectos hasta un 5to nivel de profundidad. Participa en el BUILDER POOL y aprovecha exclusividad en cualquier tipo de inmueble. Benefíciate de un ROI del 25% trimestral y descuentos de 18% en departamentos o 16% en casas."
     },
     {
         id: 6,
@@ -87,7 +92,8 @@ export const planes = [
             "22% de descuento en la compra de un departamento o 18% en una casa"
         ],
         buttonLabel: "Elegir Plan",
-        buttonLink: "/dashboard/suscripcion"
+        buttonLink: "/dashboard/suscripcion",
+        description: "El plan Gerente Inmobiliario te permite ganar de referidos directos e indirectos hasta un 6to nivel de profundidad. Accede al BUILDER POOL y participa en cualquier tipo de inmueble con un ROI del 30% trimestral. Obtén descuentos del 22% en departamentos o 18% en casas."
     },
     {
         id: 7,
@@ -102,6 +108,7 @@ export const planes = [
             "25% de descuento en la compra de un departamento o 22% en una casa"
         ],
         buttonLabel: "Elegir Plan",
-        buttonLink: "/dashboard/suscripcion"
+        buttonLink: "/dashboard/suscripcion",
+        description: "El plan Director Inmobiliario ofrece ganancias de referidos directos e indirectos hasta un 7mo nivel de profundidad. Participa en el BUILDER POOL con exclusividad en cualquier inmueble y disfruta de un ROI del 40% trimestral. Obtén descuentos de 25% en departamentos o 22% en casas."
     }
 ];
