6601caf8dd0a10873b654d06ca7715cdc03a6962
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-18 14:20:49 -0500

    refactor: Update ContactInfo component to use placeholder text for input fields

--- components/dashboard/perfil/ContactInfo.tsx
+++ components/dashboard/perfil/ContactInfo.tsx
@@ -6,11 +6,11 @@ const ContactInfo = ({ formValues, handleChange, isEditing }: any) => (
     <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
         <LabelInputContainer>
             <Label htmlFor="email">Correo Electrónico</Label>
-            <Input id="email" placeholder="micorreo@gmail.com" type="text" value={formValues.email} onChange={handleChange} readOnly={!isEditing} />
+            <Input id="email" placeholder="Correo electrónico" type="email" value={formValues.email} onChange={handleChange} readOnly={!isEditing} />
         </LabelInputContainer>
         <LabelInputContainer>
             <Label htmlFor="phoneNumber">Número Telefónico</Label>
-            <Input id="phoneNumber" placeholder="+51 987 654 321" type="text" value={formValues.phoneNumber} onChange={handleChange} readOnly={!isEditing} />
+            <Input id="phoneNumber" placeholder="Número telefónico" type="text" value={formValues.phoneNumber} onChange={handleChange} readOnly={!isEditing} />
         </LabelInputContainer>
     </div>
 );
