531025fdddb680de62e2754221c8c1c858c27bcd
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-18 14:20:55 -0500

    refactor: Update UserInfo component to use placeholder text for input fields

--- components/dashboard/perfil/UserInfo.tsx
+++ components/dashboard/perfil/UserInfo.tsx
@@ -3,22 +3,19 @@ import { Label, LabelInputContainer } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
 const UserInfo = ({ formValues, handleChange, isEditing }: any) => (
-    <div className="flex flex-col py-3">
+    <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 py-3 md:space-x-2 mb-4">
         <LabelInputContainer>
-            <Label htmlFor="username">Usuario*</Label>
-            <Input id="username" placeholder="Mafer" type="text" value={formValues.username} onChange={handleChange} readOnly={!isEditing} />
+            <Label htmlFor="username">Usuario</Label>
+            <Input id="username" placeholder="Nombre de usuario" type="text" value={formValues.username} readOnly />
         </LabelInputContainer>
-
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 py-3 md:space-x-2 mb-4">
-            <LabelInputContainer>
-                <Label htmlFor="firstName">Nombres</Label>
-                <Input id="firstName" placeholder="Maria" type="text" value={formValues.firstName} onChange={handleChange} readOnly={!isEditing} />
-            </LabelInputContainer>
-            <LabelInputContainer>
-                <Label htmlFor="lastName">Apellidos</Label>
-                <Input id="lastName" placeholder="Moreno" type="text" value={formValues.lastName} onChange={handleChange} readOnly={!isEditing} />
-            </LabelInputContainer>
-        </div>
+        <LabelInputContainer>
+            <Label htmlFor="firstName">Nombres</Label>
+            <Input id="firstName" placeholder="Nombres" type="text" value={formValues.firstName} onChange={handleChange} readOnly={!isEditing} />
+        </LabelInputContainer>
+        <LabelInputContainer>
+            <Label htmlFor="lastName">Apellidos</Label>
+            <Input id="lastName" placeholder="Apellidos" type="text" value={formValues.lastName} onChange={handleChange} readOnly={!isEditing} />
+        </LabelInputContainer>
     </div>
 );
 
