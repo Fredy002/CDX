6d9ccb93bcdf04dd2cbf2c19b6be5da95fdfa984
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-18 12:09:29 -0500

    refactor: Update database configuration for local development

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,52 +1,6 @@
+import db, { cors, testConnection } from '@/utils/db/db';
 import { NextApiRequest, NextApiResponse } from 'next';
-import mysql from 'mysql2/promise';
 
-// Configuración de la base de datos
-
-// const db = mysql.createPool({
-//     host: 'auth-db1436.hstgr.io',
-//     user: 'u408348937_cdx',
-//     password: 'Jorgitotuterror666',
-//     database: 'u408348937_dinsy',
-//     port: 3306,
-// });
-
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'mydatabase',
-    port: 3306,
-});
-
-async function testConnection() {
-    try {
-        const connection = await db.getConnection();
-        await connection.ping();
-        connection.release();
-        return true;
-    } catch (error) {
-        console.error('Database connection failed:', error);
-        return false;
-    }
-}
-
-const cors = (req: NextApiRequest, res: NextApiResponse, next: Function) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-
-    next();
-};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     cors(req, res, () => { });
--- pages/api/properties.ts
+++ pages/api/properties.ts
@@ -1,31 +1,5 @@
+import db, { cors } from '@/utils/db/db';
 import { NextApiRequest, NextApiResponse } from 'next';
-import mysql from 'mysql2/promise';
-
-// Configuración de la base de datos
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'mydatabase',
-    port: 3306,
-});
-
-const cors = (req: NextApiRequest, res: NextApiResponse, next: Function) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-
-    next();
-};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     cors(req, res, () => { });
