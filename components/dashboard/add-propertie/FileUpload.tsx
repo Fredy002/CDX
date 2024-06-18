f38ee87bcc3165030000a89549ff664d29591643
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-18 12:04:20 -0500

    refactor: Update FileUpload component to handle image uploading and removal

--- components/dashboard/add-propertie/FileUpload.tsx
+++ components/dashboard/add-propertie/FileUpload.tsx
@@ -1,28 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 import { Input } from '@/components/ui/input';
 import { Label, LabelInputContainer } from '@/components/ui/label';
 import React, { useState } from 'react';
 
+type FileUploadProps = {
+    formValues: {
+        imageUrls: string[];
+    };
+    handleImageChange: (imageUrls: string[]) => void;
+};
 
-const FileUpload = () => {
-    const [imageUrls, setImageUrls] = useState<string[]>([]);
+const FileUpload: React.FC<FileUploadProps> = ({ formValues, handleImageChange }) => {
     const [currentImageUrl, setCurrentImageUrl] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
 
-    const handleImageChange = () => {
+    const handleAddImage = () => {
+        if (!currentImageUrl) {
+            setError('Por favor ingrese una URL de imagen válida.');
+            return;
+        }
+
+        const validImageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
+        const isValidUrl = currentImageUrl.startsWith('https://');
+        const isValidExtension = validImageExtensions.some(ext => currentImageUrl.endsWith(ext));
+
+        if (!isValidUrl || !isValidExtension) {
+            setError('Tipo de imagen no compatible.');
+            return;
+        }
+
         setLoading(true);
         const img = new Image();
         img.src = currentImageUrl;
         img.onload = () => {
             setLoading(false);
-            setImageUrls((prevUrls) => [...prevUrls, currentImageUrl]);
+            handleImageChange([...formValues.imageUrls, currentImageUrl]);
             setCurrentImageUrl('');
+            setError(null);
+        };
+        img.onerror = () => {
+            setLoading(false);
+            setError('No se pudo cargar la imagen. Por favor, verifique la URL.');
         };
     };
 
     const handleRemoveImage = (index: number) => {
-        setImageUrls((prevUrls) => prevUrls.filter((_, i) => i !== index));
+        const updatedUrls = formValues.imageUrls.filter((_, i) => i !== index);
+        handleImageChange(updatedUrls);
     };
 
     return (
@@ -35,7 +62,7 @@ const FileUpload = () => {
                             <span>Loading ....</span>
                         </div>
                     ) : (
-                        imageUrls.map((url, index) => (
+                        formValues.imageUrls.map((url, index) => (
                             <div key={index} className="relative w-64 h-64 border border-gray-300 rounded-md overflow-hidden">
                                 <img src={url} alt={`Selected ${index}`} className='w-full h-full object-cover' />
                                 <button onClick={() => handleRemoveImage(index)} className="absolute top-2 right-2 bg-red-500 text-white rounded-2xl p-1">x</button>
@@ -43,11 +70,12 @@ const FileUpload = () => {
                         ))
                     )}
                 </div>
+                {error && <div className="text-red-500 mt-2">{error}</div>}
             </div>
             <div className='flex flex-row flex-start gap-5 items-center py-2'>
                 <Label>Cargar imagen:</Label>
                 <Input type='text' value={currentImageUrl} onChange={(e) => setCurrentImageUrl(e.target.value)} />
-                <button className='bg-blue-500 text-white p-2 rounded-xl' onClick={handleImageChange}>Añadir Foto</button>
+                <button className='bg-blue-500 text-white p-2 rounded-xl' onClick={handleAddImage}>Añadir Foto</button>
             </div>
         </div>
     );
